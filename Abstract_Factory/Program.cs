2dc1cd5093412b35192e52e17320ea2a3c6fa38e
Nick Kuzmin <nickkuzmin1301@gmail.com>
2021-05-03 12:03:23 +0300

    Добавлена возможность указать вариант сортироки, названия входного и исходного файлов в качестер входных параметров

--- Abstract_Factory/Program.cs
+++ Abstract_Factory/Program.cs
@@ -7,18 +7,25 @@ namespace Abstract_Factory
     {
         public static void Main(string[] args)
         {
-            string inputFilePath = "input.txt";
-            string outputFilePath = "output.txt";
+            // Получает название метода сортировки
+            string sortingAlgorithm = args.Length > 0 && args[0].Length > 0 ? args[0].ToLower() : "insertion";
+            // Получает название файла, из которого будет производиться чтение
+            string inputFilePath = (args.Length > 1 && args[1].Length > 0) ? args[1] :"input.txt";
+            // Получает название файла, в который будет производиться запись
+            string outputFilePath = (args.Length > 2 && args[2].Length > 0) ? args[2] : "output.txt";
 
             FileOperator fileOperator = new FileOperator();
             
             string fileContent = fileOperator.ReadTextFromFile(inputFilePath);
             int[] unsortedArray = fileContent.Split(' ').Select(n => Convert.ToInt32(n)).ToArray();
-
+            
+            // Определение алгоритма сортировки
             SorterManager manager = new SorterManager();
-            var sorter = manager.GetSorter("insertion");
+            var sorter = manager.GetSorter(sortingAlgorithm);
+            // Сортировка массива
             int[] resultArray = (int[]) sorter.Sort(unsortedArray);
 
+            // Запись отсортированного массива в файл
             string resultArrayAsString = String.Join(", ", resultArray);
             fileOperator.WriteTextToFile(outputFilePath, resultArrayAsString);
         }
